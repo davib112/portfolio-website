b0f809cc2ee0f84825ef33bb2fe8960aad37d821
Carl Nordholm <nordholm.carl@gmail.com>
2026-02-22 17:25:23 +0100

    fixed internal a to Link

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,6 @@
 import SiteHeader from "@/components/SiteHeader";
 import errorStyle from "@/pages/error.module.css"
+import Link from "next/link";
 
 export default function Custom404() {
    return (
@@ -9,7 +10,7 @@ export default function Custom404() {
          <div className={errorStyle.errorParent}>
             <h1>HERE WERE DRAGONS - 404</h1>
             <p>
-               The dragons are not around any longer but some other intresting stuff still is, feel free to check out some of my other pages. <a href="/pong">Maybe play some pong</a> or <a href="/">go back to safe waters.</a>
+               The dragons are not around any longer but some other intresting stuff still is, feel free to check out some of my other pages. <Link href="/pong">Maybe play some pong</Link> or <Link href="/">go back to safe waters.</Link>
             </p>
          </div>
       </div>
