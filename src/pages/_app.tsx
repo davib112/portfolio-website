d685f9b0cd4c3b7eb84970143fd9a7175725f697
Carl Nordholm <nordholm.carl@gmail.com>
2026-02-22 14:18:21 +0100

    fixed let to const for const varible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
-	let backgroundPath: string = `${basePath}/media/test/limestone.jpg`
+	const backgroundPath: string = `${basePath}/media/test/limestone.jpg`
 	return (
 		<div style={{ '--background-path': `url(${backgroundPath})` } as React.CSSProperties}>
 			<Component {...pageProps} />
