f2ec73cb47c839583de5b23ee18044cb7f95f9dc
Carl Nordholm <nordholm.carl@gmail.com>
2026-02-22 16:32:23 +0100

    update NEXT_PUBLIC_BASE_PATH for prod paths

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
+const internalBasePath = isProd ? '/portfolio-website' : '';
 
 const nextConfig: NextConfig = {
 	output: 'export',
 	distDir: 'build',
 	basePath: isProd ? '/portfolio-website' : '',
 	assetPrefix: isProd ? '/portfolio-website/': '', // include extra folder
+	env:{
+		NEXT_PUBLIC_BASE_PATH: isProd ? '/portfolio-website' : '',
+	}
 };
 
 export default nextConfig;
