c98ce18b86bf0e7322d7472609eea3008788577b
Carl Nordholm <nordholm.carl@gmail.com>
2026-03-14 23:34:23 +0100

    fixed broken next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
 	output: 'export',
 	distDir: 'build',
+	basePath: '',
+	assetPrefix: '',
+	env: {
+		NEXT_PUBLIC_BASE_PATH: '',
+	}
+
 	//basePath: isProd ? '/portfolio-website' : '',
 	//assetPrefix: isProd ? '/portfolio-website/': '', // include extra folder
 	//env:{
